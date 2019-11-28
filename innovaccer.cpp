d3f3fe998ee9917904fe77a816e0ba17fd3780dd
Abhishek Mittal <32011708+abhishek16101998@users.noreply.github.com>
2019-11-28 11:05:16 +0530

    Add files via upload

---
+++ innovaccer.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string name,email,checkin,checkout,mobile,host,address;
+	
+	cin>>name>>email>>checkin>>checkout>>host>>address;
+	
+	cout<<name<<endl<<email<<endl<<checkin<<endl<<checkout<<endl<<host<<endl<<address<<endl;
+	
+}
